297aeaf79b149d40eeeda1c2174fdfb5a279dc40
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2021-04-12 17:17:20 +1000

    Added some more test

--- test/test_qcost.py
+++ test/test_qcost.py
@@ -23,9 +23,13 @@ def test_qcost():
     assert(qcost.qcost('normal', 8, '4GB', '1:00:00') == 8*2)
     assert(qcost.qcost('normal', 4, '32GB', '1:00:00') == 8*2)
     assert(qcost.qcost('normal', 4, '32GB', '2:30:00') == 8*2*2.5)
+    assert(qcost.qcost('normal', 1, '8192MB', '1:00:00') == 2*2)
+
     assert(qcost.qcost('express', 4, '4GB', '1:00:00') == 4*6)
 
     assert(qcost.qcost('normalbw', 4, '4GB', '1:00:00') == 4*1.25)
     assert(qcost.qcost('normalbw', 4, '32GB', '1:00:00') == 4*1.25)
     assert(qcost.qcost('normalbw', 4, '256GB', '1:00:00') == 28*1.25)
+
     assert(qcost.qcost('normalsl', 4, '4GB', '1:00:00') == 4*1.5)
+    assert(qcost.qcost('normalsl', 4, '30GB', '1:00:00') == 5*1.5)
